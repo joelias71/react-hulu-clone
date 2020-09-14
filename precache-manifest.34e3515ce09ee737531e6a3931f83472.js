f178ddc5518c6c1fceba13f17f978eb10a2f7d3b
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-14 16:15:22 -0500

    Updates

---
+++ precache-manifest.34e3515ce09ee737531e6a3931f83472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23346375e02d369e55c879b3c0514964",
+    "url": "/react-hulu-clone/index.html"
+  },
+  {
+    "revision": "2a2462d767f855dfbb48",
+    "url": "/react-hulu-clone/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7fd5d4dcb7c2331c198a",
+    "url": "/react-hulu-clone/static/js/2.b02c4f1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hulu-clone/static/js/2.b02c4f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2462d767f855dfbb48",
+    "url": "/react-hulu-clone/static/js/main.78182ef1.chunk.js"
+  },
+  {
+    "revision": "9e661d1aa9a46daade1d",
+    "url": "/react-hulu-clone/static/js/runtime-main.5b55872a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-hulu-clone/static/media/logo.ee7cd8ed.svg"
+  }
+]);
