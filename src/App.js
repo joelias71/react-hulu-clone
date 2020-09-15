9bb06c70f9a4578f1370fa2921a55754c9820707
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-14 21:00:22 -0500

    feat(Nav) Add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Nav />
     </div>
   );
 }
