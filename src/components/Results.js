d9a018763c2b0cef2853de6ea307749203a736f5
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-19 18:25:35 -0500

    feat(data) fetch data from API

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Results.css'
 import VideoCard from './VideoCard'
+import axios from '../data/axios'
+import requests from '../data/requests'
 
 function Results() {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get(requests.fetchActionMovies)
+            setMovies(req.data.results)
+            return req
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div className='results' >
-            <VideoCard />
+            {movies.map(movie => <VideoCard movie={movie} />)}
         </div>
     )
 }
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import './VideoCard.css'
+import TextTruncate from 'react-text-truncate'
+import { ThumbUpSharp } from '@material-ui/icons'
+
+const base_url = 'https://image.tmdb.org/t/p/original/'
 
 function VideoCard({ movie }) {
     return (
         <div className='videoCard' >
-            <img src='https://fondosmil.com/fondo/12733.jpg' alt='' />
-            <p>This is a film about coding</p>
-            <h2>Movie title</h2>
-            <p>Number of likes ...</p>
+            <img src={ `${base_url}${movie.backdrop_path || movie.poster_path}` } alt='Movie poster' />
+            <TextTruncate 
+                line={1}
+                element='p'
+                truncate='...'
+                text={movie.overview}
+            />
+            <h2>{movie.title || movie.original_name}</h2>
+            <p className='videoCard__stats' >
+                {movie.release_date || movie.first_air_date}{' • '}
+                <ThumbUpSharp />{' '}
+                {movie.vote_count}
+            </p>
         </div>
     )
 }
