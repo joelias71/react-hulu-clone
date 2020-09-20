aebf68df3c342a61c886eaad7a16f64bff5f72a2
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-19 21:54:41 -0500

    feat(nav) Add routing in nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Results from './components/Results'
+import requests from './data/requests'
 
 function App() {
+
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending)
+
   return (
     <div className="app">
       <Header />
-      <Nav />
-      <Results />
+      <Nav setSelectedOption={setSelectedOption} />
+      <Results selectedOption={selectedOption} />
     </div>
   );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -2,25 +2,24 @@ import React, { useState, useEffect } from 'react'
 import './Results.css'
 import VideoCard from './VideoCard'
 import axios from '../data/axios'
-import requests from '../data/requests'
 
-function Results() {
+function Results({ selectedOption }) {
 
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
         async function fetchData() {
-            const req = await axios.get(requests.fetchActionMovies)
+            const req = await axios.get(selectedOption)
             setMovies(req.data.results)
             return req
         }
 
         fetchData()
-    }, [])
+    }, [selectedOption])
 
     return (
         <div className='results' >
-            {movies.map(movie => <VideoCard movie={movie} />)}
+            {movies.map(movie => <VideoCard key={movie.id} movie={movie} />)}
         </div>
     )
 }
